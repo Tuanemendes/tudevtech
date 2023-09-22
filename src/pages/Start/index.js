b5178b0839b33ded2c45da0e568fde7f6955f0d9
Tuanemendes <tuane.belmiro@gmail.com>
2023-09-22 06:41:49 -0300

    crianção do componete post

--- src/pages/Start/index.js
+++ src/pages/Start/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Banner from '../../components/Banner'
+import styles from '../Start/Start.module.css'
+import posts from  '../../json/posts.json'
 
 const Start = () => {
   return (
     <main>
       <Banner/>
-      <h1>Inicio</h1>
+      <ul className={styles.post}>
+        {posts.map((post)=> (
+          <li key={post.id}>
+            <Post/>
+          </li>
+        ))}
+      </ul>
     </main>
 
   )
